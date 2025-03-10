5f58b4c52ba9c13d5176d4c961f62c1c79023faf
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-03-11 08:55:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Slackside80Pop
+
+# 『Slackside-Regular』について
+[SlacksideOne](https://github.com/ManiackersDesign/slackside) に不足している漢字などのデータを [HachiMaruPop](https://github.com/noriokanisawa/HachiMaruPop) で補ったフォントです。
+名前はそれぞれのフォント名由来です。
+
+# SIL Open Font License Version 1.1 ライセンスについて
+・個人利用・商用利用にかかわらずどなたでも無料でお使いいただけます。
+
+・ゲームやアプリなどへの組み込みやWebフォントとしての利用も可能です。
+
+・このフォントをアレンジし、派生フォントを作ることもできます。
+ただし、配布の際はSIL Open Font Licenseに基づいてリリースする必要があります。
+
+・SILライセンスについて詳しくはライセンス原文日本語サイト
+（ https://ja.osdn.net/projects/opensource/wiki/SIL_Open_Font_License_1.1 ）
+または同梱の「OFL.txt」（英語）をご確認ください。
+
+# 収録文字
+以下をご確認ください。
+- [SlacksideOne](https://github.com/ManiackersDesign/slackside#readme)
+- [HachiMaruPop](https://github.com/noriokanisawa/HachiMaruPop#readme)
+
+# 禁止事項
+・「SIL Open Font License Version 1.1」以外のライセンスで再配布すること。
+・フォントファイル自体を単体で販売すること。
+
+# その他
+フォントテスト: [テストページ](https://otoneko1102.github.io/Slackside80Pop)
+
+# 更新履歴
+2025年3月11日 バージョン 1.0
---
+++ marge.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const path = require("path");
+const { Font } = require("fonteditor-core");
+
+const aFontPath = path.join(__dirname, "./fonts/SlacksideOne.ttf");
+const bFontPath = path.join(__dirname, "./fonts/HachiMaruPop.ttf");
+const outputFontPath = path.join(__dirname, "./Slackside80Pop.ttf");
+
+async function mergeFonts() {
+  try {
+    const aBuffer = fs.readFileSync(aFontPath);
+    const bBuffer = fs.readFileSync(bFontPath);
+
+    const aFont = Font.create(aBuffer, { type: "ttf" });
+    const bFont = Font.create(bBuffer, { type: "ttf" });
+
+    const aData = aFont.get();
+    const bData = bFont.get();
+
+    const aGlyf = aData.glyf || [];
+    const bGlyf = bData.glyf || [];
+
+    console.log("フォント A:", aGlyf.length, "グリフ");
+    console.log("フォント B:", bGlyf.length, "グリフ");
+
+    const aUnicodeSet = new Set(aGlyf.map(g => g.unicode).flat().filter(Boolean));
+    const missingGlyphs = bGlyf.filter(g => g.unicode && !aUnicodeSet.has(g.unicode[0]));
+
+    console.log("追加するグリフ数:", missingGlyphs.length);
+
+    for (let glyph of missingGlyphs) {
+      if (!glyph.contours) glyph.contours = [];
+      if (!glyph.advanceWidth) glyph.advanceWidth = 500;
+    }
+
+    aGlyf.push(...missingGlyphs);
+
+    aData.maxp.numGlyphs = aGlyf.length;
+
+    if (!aData.cmap) {
+      aData.cmap = {};
+    }
+
+    for (let glyph of missingGlyphs) {
+      if (glyph.unicode && glyph.unicode.length > 0) {
+        aData.cmap[glyph.unicode[0]] = aGlyf.indexOf(glyph);
+      }
+    }
+
+    aData.head.checksumAdjustment = 0;
+    aData.post.format = 2;
+    aFont.set(aData);
+
+    const mergedBuffer = aFont.write({ type: "ttf" });
+
+    fs.writeFileSync(outputFontPath, Buffer.from(mergedBuffer));
+    console.log(`フォントを作成しました: ${outputFontPath}`);
+  } catch (err) {
+    console.error("エラー:", err);
+  }
+}
+
+mergeFonts();
